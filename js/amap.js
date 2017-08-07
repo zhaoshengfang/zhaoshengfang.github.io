939fd66c8a5117fc28face3d70fd06a30bc0f4d6
zhaoshengfang <1469593898@qq.com>
2017-08-07 21:12:43 +0800

    Add files via upload

---
+++ js/amap.js
@@ -0,0 +1,15 @@
+/**
+ * Created by DZX on 2017/8/6.
+ */
+/**
+ * Created with WebStorm && Sublime Text 3
+ * Date: 2015/10/29 14:04
+ */
+window.onload = function() {
+    map.plugin(["AMap.ToolBar"], function() {
+        map.addControl(new AMap.ToolBar());
+    });
+    if(location.href.indexOf('&guide=1')!==-1){
+        map.setStatus({scrollWheel:false})
+    }
+}
